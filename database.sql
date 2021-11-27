4cf4f3c2060e1654bfdf11c26614983f4fe67ad4
DeXotik <95002001+DeXotik@users.noreply.github.com>
2021-11-28 00:30:55 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -8,26 +8,30 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 
+DROP TABLE IF EXISTS `actions`;
 CREATE TABLE `actions` (
   `actionID` int NOT NULL,
   `type` int NOT NULL,
   `value1` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `value2` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `value2` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `value3` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `actionDate` int NOT NULL,
-  `IP` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '127.0.0.1'
+  `IP` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '127.0.0.1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+DROP TABLE IF EXISTS `bans`;
 CREATE TABLE `bans` (
   `banID` int NOT NULL,
-  `IP` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `reason` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `IP` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `reason` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `time` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
   `commentID` int NOT NULL,
   `levelID` int NOT NULL,
-  `comment` text COLLATE utf8_unicode_ci NOT NULL,
+  `comment` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `likes` int NOT NULL DEFAULT '0',
   `spam` int NOT NULL DEFAULT '0',
   `uploadDate` int NOT NULL,
@@ -36,6 +40,7 @@ CREATE TABLE `comments` (
   `deleted` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+DROP TABLE IF EXISTS `levels`;
 CREATE TABLE `levels` (
   `levelID` int NOT NULL,
   `levelName` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -43,6 +48,8 @@ CREATE TABLE `levels` (
   `downloads` int NOT NULL DEFAULT '0',
   `likes` int NOT NULL DEFAULT '0',
   `difficulty` int NOT NULL DEFAULT '0',
+  `demon` int NOT NULL DEFAULT '0',
+  `stars` int NOT NULL DEFAULT '0',
   `rated` int NOT NULL DEFAULT '0',
   `levelVersion` int NOT NULL DEFAULT '1',
   `levelLength` int NOT NULL DEFAULT '0',
@@ -56,25 +63,35 @@ CREATE TABLE `levels` (
   `deleted` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+DROP TABLE IF EXISTS `roleassign`;
 CREATE TABLE `roleassign` (
   `roleassignID` int NOT NULL,
   `roleID` int NOT NULL,
   `userID` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles` (
   `roleID` int NOT NULL,
-  `roleName` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `roleName` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `commandDelete` int NOT NULL DEFAULT '0',
   `commandRate` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `userID` int NOT NULL,
-  `accountID` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `userName` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Unknown',
+  `accountID` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `userName` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Unknown',
+  `stars` int NOT NULL DEFAULT '0',
+  `demons` int NOT NULL DEFAULT '0',
+  `creatorPoints` int NOT NULL DEFAULT '0',
+  `icon` int NOT NULL DEFAULT '0',
+  `color1` int NOT NULL DEFAULT '0',
+  `color2` int NOT NULL DEFAULT '0',
   `lastActive` int NOT NULL DEFAULT '0',
-  `IP` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '127.0.0.1'
+  `IP` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '127.0.0.1',
+  `scoreBan` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
