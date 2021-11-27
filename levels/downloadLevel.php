839737d3379b0a91788a5dd0d5239686f2a11c94
DeXotik <95002001+DeXotik@users.noreply.github.com>
2021-11-28 00:09:43 +0200

    Update downloadLevel.php

--- levels/downloadLevel.php
+++ levels/downloadLevel.php
@@ -9,7 +9,7 @@
 
     $ip = $f->getIP();
 
-    // Data - version 1.2
+    // Data - version 1.0
     $levelID = isset($_POST["levelID"]) ? $f->checkNum($_POST["levelID"]) : "";
     // $inc = isset($_POST["inc"]) ? $f->checkNum($_POST["inc"]) : 1;
 
@@ -34,19 +34,25 @@
             }
 
             $levelString = file_get_contents(dirname(__FILE__)."/data/$levelID");
+            $levelDesc = base64_decode($level['levelDesc']);
 
+            // Output - version 1.0
             $lvlString = "1:".$level["levelID"]; // level ID
             $lvlString .= ":2:".$level["levelName"]; // level name
-            $lvlString .= ":3:".base64_decode($level['levelDesc']); // level description
+            $lvlString .= ":3:".$levelDesc; // level description
             $lvlString .= ":4:".$levelString; // level data
             $lvlString .= ":5:".$level["levelVersion"]; // level version
             $lvlString .= ":6:".$level["userID"]; // user ID
-            $lvlString .= ":8:10:9:".$f->getRealDifficulty($level["difficulty"]); // level difficulty
+            $lvlString .= ":8:10:9:".($level["difficulty"] * 10); // level difficulty
             $lvlString .= ":10:".$level["downloads"]; // downloads on level
             $lvlString .= ":12:".$level["audioTrack"]; // audiotrack on level
             $lvlString .= ":13:".$level["gameVersion"]; // game version
             $lvlString .= ":14:".$level["likes"]; // likes on level
             $lvlString .= ":15:".$level["levelLength"]; // level length
+            $lvlString .= ":16:0"; // likes - count = likes on level
+            // Output - version 1.3
+            $lvlString .= ":17:".$level["demon"]; // is demon
+            $lvlString .= ":18:".$level["stars"]; // stars
 
             echo $lvlString;
         } else exit("-1");
