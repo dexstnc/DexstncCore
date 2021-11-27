3c960979b92a9e2ee0a562db650f760b2c8b6331
DeXotik <95002001+DeXotik@users.noreply.github.com>
2021-11-28 00:06:48 +0200

    Update commands.php

--- include/commands.php
+++ include/commands.php
@@ -28,29 +28,63 @@
                     break;
                 case "!rate":
                     if($f->checkPossibility($userID, "commandRate")){
+                        require "../config/settings.php";
+
+                        if(!empty($commentArray[2]) AND is_numeric($commentArray[2])){
+                            $stars = $commentArray[2];
+                        }
+
+                        $demon = 0;
                         switch($commentArray[1]){
                             case "easy":
                                 $difficulty = 1;
+                                $stars = 2;
                                 break;
                             case "normal":
                                 $difficulty = 2;
+                                $stars = 3;
                                 break;
                             case "hard":
                                 $difficulty = 3;
+                                $stars = 4;
                                 break;
                             case "harder":
                                 $difficulty = 4;
+                                $stars = 6;
                                 break;
                             case "insane":
                                 $difficulty = 5;
+                                $stars = 8;
+                                break;
+                            case "demon":
+                                $difficulty = 5;
+                                $demon = 1;
+                                $stars = 10;
                                 break;
                             default: return false;
                         }
 
-                        $query = $db->prepare("UPDATE levels SET difficulty = :difficulty, rated = 1, rateDate = :time WHERE levelID = :levelID AND deleted = 0");
-                        $query->execute([':difficulty' => $difficulty, ':time' => time(), ':levelID' => $levelID]);
-                        $query = $db->prepare("INSERT INTO actions (type, value1, value2, IP, actionDate) VALUES (5, :levelID, :difficulty, :ip, :time)");
-                        $query->execute([':levelID' => $levelID, ':difficulty' => $difficulty, ':ip' => $ip, ':time' => time()]);
+                        // Command rate limit
+                        if($commandRateLimit === true){
+                            if($commandRateLimitCheckStars){
+                                if($commentArray[1] == "easy" AND $stars != 2) return false;
+                                if($commentArray[1] == "normal" AND $stars != 3) return false;
+                                if($commentArray[1] == "hard" AND ($stars != 4 OR $stars != 5)) return false;
+                                if($commentArray[1] == "harder" AND ($stars != 6 OR $stars != 7)) return false;
+                                if($commentArray[1] == "insane" AND ($stars != 8 OR $stars != 9)) return false;
+                                if($commentArray[1] == "demon" AND $stars != 10) return false;
+                            }
+                        }
+                        if($commandRateLimitStars === true){
+                            if($commandRateLimitMinStars <= $commandRateLimitMaxStars){
+                                if($stars < $commandRateLimitMinStars OR $stars > $commandRateLimitMaxStars) return false;
+                            }
+                        }
+
+                        $query = $db->prepare("UPDATE levels SET difficulty = :difficulty, demon = :demon, stars = :stars, rated = 1, rateDate = :time WHERE levelID = :levelID AND deleted = 0");
+                        $query->execute([':difficulty' => $difficulty, ':demon' => $demon, ':stars' => $stars, ':time' => time(), ':levelID' => $levelID]);
+                        $query = $db->prepare("INSERT INTO actions (type, value1, value2, value3, IP, actionDate) VALUES (5, :levelID, :difficulty, :stars, :ip, :time)");
+                        $query->execute([':levelID' => $levelID, ':difficulty' => $difficulty, ':stars' => $stars, ':ip' => $ip, ':time' => time()]);
 
                         return true;
                     }
