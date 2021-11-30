133236d6e7da16f7fd74735677ad6ccfccb4cc53
DeXotik <95002001+DeXotik@users.noreply.github.com>
2021-11-30 22:16:03 +0200

    Update getScores.php

--- scores/getScores.php
+++ scores/getScores.php
@@ -1,10 +1,8 @@
 <?php
-    chdir(dirname(__FILE__));
-
-    include "../include/database.php";
-    require_once "../include/functions.php";
+    include dirname(__FILE__)."/../include/database.php";
+    include dirname(__FILE__)."/../config/settings.php";
+    require_once dirname(__FILE__)."/../include/functions.php";
     $f = new Functions();
-    require_once "../config/settings.php";
 
     $f->checkBanIP();
 
@@ -26,6 +24,8 @@
                 $query->execute();
                 $users = $query->fetchAll();
 
+                if(count($users) == 0) exit("1:No players with more than zero stars:2:0:3:0:4:0:6:0:7:0:8:0:9:0:10:0:11:3");
+
                 break;
             case "relative":
                 $scoreCount = floor($scoreCount / 2);
@@ -40,36 +40,36 @@
                     $query = $db->prepare("SET @rownum1 := 0;"); $query->execute();
                     $query = $db->prepare("SET @rownum2 := 0;"); $query->execute();
                     $query = $db->prepare("SELECT a.* FROM ((
-                        SELECT a.* FROM (SELECT @rownum1 := @rownum2 + 1 AS rownum, $usersTable) AS a WHERE a.rownum <= :rownum ORDER BY rownum DESC LIMIT $scoreCount
+                        SELECT b.* FROM (SELECT @rownum1 := @rownum2 + 1 AS rownum, $usersTable) AS b WHERE b.rownum < :rownum ORDER BY b.rownum DESC LIMIT $scoreCount
                     ) UNION (
-                        SELECT a.* FROM (SELECT @rownum2 := @rownum2 + 1 AS rownum, $usersTable) AS a WHERE a.rownum > :rownum ORDER BY rownum ASC LIMIT $scoreCount
-                    )) AS a ORDER BY rownum ASC");
+                        SELECT c.* FROM (SELECT @rownum2 := @rownum2 + 1 AS rownum, $usersTable) AS c WHERE c.rownum >= :rownum ORDER BY c.rownum ASC LIMIT $scoreCount
+                    )) AS a ORDER BY a.rownum ASC");
                     $query->execute([':rownum' => $rownum]);
-                    $users =  $query->fetchAll();
+                    $users = $query->fetchAll();
                 } else {
                     $users = [];
                 }
+
+                if(count($users) == 0) exit("1:No players:2:0:3:0:4:0:6:0:7:0:8:0:9:0:10:0:11:3");
 
                 break;
             default: exit("-1");
         }
 
         $scoreString = "";
-        if(count($users) > 0){
-            foreach($users AS $user){
-                $scoreString .= "1:".$user["userName"]; // userName
-                $scoreString .= ":2:".$user["userID"]; // userID
-                $scoreString .= ":3:".$user["stars"]; // stars
-                $scoreString .= ":4:".$user["demons"]; // demons
-                $scoreString .= ":6:".$user["rownum"]; // rank
-                $scoreString .= ":7:".$user["accountID"]; // accountID
-                $scoreString .= ":8:".$user["creatorPoints"]; // creator points
-                $scoreString .= ":9:".$user["icon"]; // icon
-                $scoreString .= ":10:".$user["color1"]; // color1
-                $scoreString .= ":11:".$user["color2"]; // color2
-                $scoreString .= "|";
-            }
-        } else $scoreString .= "1:No players with more than zero stars:2:0:3:0:4:0:6:0:7:0:8:0:9:0:10:0:11:3|";
+        foreach($users AS $user){
+            $scoreString .= "1:".$user["userName"]; // userName
+            $scoreString .= ":2:".$user["userID"]; // userID
+            $scoreString .= ":3:".$user["stars"]; // stars
+            $scoreString .= ":4:".$user["demons"]; // demons
+            $scoreString .= ":6:".$user["rownum"]; // rank
+            $scoreString .= ":7:".$user["accountID"]; // accountID
+            $scoreString .= ":8:".$user["creatorPoints"]; // creator points
+            $scoreString .= ":9:".$user["icon"]; // icon
+            $scoreString .= ":10:".$user["color1"]; // color1
+            $scoreString .= ":11:".$user["color2"]; // color2
+            $scoreString .= "|";
+        }
 
         $scoreString = substr($scoreString, 0, -1);
         echo $scoreString;
