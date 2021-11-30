91cee263e6dad9a17d9127d7d74004719f771002
DeXotik <95002001+DeXotik@users.noreply.github.com>
2021-11-30 22:04:46 +0200

    Create index.php

---
+++ cron/index.php
@@ -0,0 +1,26 @@
+<?php
+    set_time_limit(0);
+
+    $time = time();
+
+    if(file_exists(dirname(__FILE__)."/include/logs.php")){
+        include dirname(__FILE__)."/include/logs.php";
+
+        $newTime = $time - 300; // Run CRON only once every 5 minutes
+        if($lastStart > $newTime) exit('Please wait '.($lastStart - $newTime).' seconds and start again.');
+    }
+
+    $file = fopen(dirname(__FILE__)."/include/logs.php", "w");
+    fwrite($file, '<? $lastStart = '.$time.' ?>');
+    fclose($file);
+
+    echo "CRON started<hr>";
+
+    include "include/fixRatedLevels.php";
+    echo "<hr>"; ob_flush(); flush();
+
+    include "include/autoCreatorPoints.php";
+    echo "<hr>"; ob_flush(); flush();
+
+    echo "CRON finished<br>";
+?>
