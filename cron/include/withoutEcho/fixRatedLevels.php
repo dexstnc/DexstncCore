e3cc8694e83e413d916832473a1603797072f507
DeXotik <95002001+DeXotik@users.noreply.github.com>
2021-12-01 17:08:30 +0200

    Add files via upload

---
+++ cron/include/withoutEcho/fixRatedLevels.php
@@ -0,0 +1,9 @@
+<?php
+    include dirname(__FILE__)."/../../../include/database.php";
+
+    $query = $db->prepare("UPDATE levels SET rated = 0 WHERE stars = 0 AND rated = 1");
+    $query->execute();
+
+    $query = $db->prepare("UPDATE levels SET rated = 1 WHERE stars > 0 AND rated = 0");
+    $query->execute();
+?>
