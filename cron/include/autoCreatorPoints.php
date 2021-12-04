d88db4d5101640c6669f3d4266c7d72a84f2d81e
DeXotik <95002001+DeXotik@users.noreply.github.com>
2021-12-04 16:52:22 +0200

    Auto CP optimization

--- cron/include/autoCreatorPoints.php
+++ cron/include/autoCreatorPoints.php
@@ -8,32 +8,53 @@
     $query = $db->prepare("SELECT userID FROM users WHERE creatorPoints > 0");
     $query->execute();
     $users = $query->fetchAll();
-    foreach($users AS $user){
-        $query = $db->prepare("SELECT count(*) FROM levels WHERE userID = :userID");
-        $query->execute([':userID' => $user["userID"]]);
-        if($query->fetchColumn() == 0){
-            $query = $db->prepare("UPDATE users SET creatorPoints = 0 WHERE userID = :userID");
-            $query->execute([':userID' => $user["userID"]]);
-
-            echo 'Creator points removed from (userID: '.$user["userID"].')<br>';
+    $newUsers = "";
+    foreach($users AS $user) $newUsers .= $user["userID"].', ';
+    if($newUsers != ""){
+        $newUsers = substr($newUsers, 0, -2);
+        $query = $db->prepare("SELECT userID, count(*) AS count FROM levels WHERE userID IN ($newUsers) GROUP BY userID");
+        $query->execute();
+        $users = $query->fetchAll();
+        $newUsers = "";
+        foreach($users AS $user) if($user["count"] == 0) $newUsers .= $user["userID"].', ';
+        if($newUsers != ""){
+            $newUsers = substr($newUsers, 0, -2);
+            $query = $db->prepare("UPDATE users SET creatorPoints = 0 WHERE userID IN ($newUsers)");
+            $query->execute();
+            $users = explode(", ", $newUsers);
+            foreach($users AS $user) echo 'Creator points fixed from (userID: '.$user.')<br>';
         }
     }
 
     // Add creatore points
-    $query = $db->prepare("SELECT userID, sum(rated) AS rated FROM levels GROUP BY userID");
+    $query = $db->prepare("SELECT levels.userID AS userID, users.creatorPoints AS creatorPoints, sum(levels.rated) AS rated FROM levels INNER JOIN users ON levels.userID = users.userID GROUP BY userID");
     $query->execute();
     $users = $query->fetchAll();
+    $newUsers = "";
     foreach($users AS $user){
         $creatorPoints = 0;
         $creatorPoints += $user["rated"] * $ratedCPs;
-
-        $query = $db->prepare("UPDATE users SET creatorPoints = :cps WHERE userID = :userID");
-        $query->execute([':cps' => $creatorPoints, ':userID' => $user["userID"]]);
-
-        if($creatorPoints > 0){
-            echo 'The (userID: '.$user["userID"].') was given '.$creatorPoints.' creator point(-s)<br>';
-        } else {
-            echo 'Creator points removed from (userID: '.$user["userID"].')<br>';
+        if($creatorPoints != $user["creatorPoints"]) $newUsers .= $user["userID"].":".$creatorPoints.", ";
+    }
+    if($newUsers != ""){
+        $newUsers = substr($newUsers, 0, -2);
+        $users = explode(", ", $newUsers);
+        $newUsers = "";
+        foreach($users AS $user){
+            $user = explode(":", $user);
+            if($user[1] > 0){
+                $query = $db->prepare("UPDATE users SET creatorPoints = :cps WHERE userID = :userID");
+                $query->execute([':cps' => $user[1], ':userID' => $user[0]]);
+    
+                echo 'The (userID: '.$user[0].') was given '.$user[1].' creator point(-s)<br>';
+            } else $newUsers .= $user[0].', ';
+        }
+        if($newUsers != ""){
+            $newUsers = substr($newUsers, 0, -2);
+            $query = $db->prepare("UPDATE users SET creatorPoints = 0 WHERE userID IN ($newUsers)");
+            $query->execute();
+            $users = explode(", ", $newUsers);
+            foreach($users AS $user) echo 'Creator points removed from (userID: '.$user.')<br>';
         }
     }
 ?>
