1b96af9ac9973d1683ff2846711d993be4aba8d5
DeXotik <95002001+DeXotik@users.noreply.github.com>
2021-12-05 00:00:41 +0200

    Update settings.php

--- config/settings.php
+++ config/settings.php
@@ -1,13 +1,22 @@
 <?php
     $checkGameVersion = false; // false - disable game version check; true - enable game version check
-    $totalGameVersion = 4;
+    $totalGameVersion = 7;
     /* 
     GAME VERSIONS
         1 - version 1.0
         2 - version 1.1
         3 - version 1.2
         4 - version 1.3
+        5 - version 1.4
+        6 - version 1.5
+        7 - version 1.6
     */
+
+    $cronRun = 300; // Run CRON once every N seconds
+    
+    $showRatedLevelsInFeatured = false; // Show rated levels in featured
+    $ratedCPs = 1; // The number of creator points for rated level
+    $featuredCPs = 1; // The number of creator points for featured level
 
     $disabledNames = ["Player", "RobTop"]; // Disabled names
     $uniqueNames = true; // false - users can be with the same name; true - only unique names
@@ -30,22 +39,27 @@
     $likesLimitTime = 3600; // Time of counting likes for the limit (in seconds)
 
     $scoreCount = 50; // Number of positions in the scores
+    /*
+        50 - version 1.3
+    */
     $scoreLimit = true; // false - disable score limit; true - enable score limit
-    $scoreLimitStars = 55 + 20; // Additional limit for stars
+    $scoreLimitStars = 102 + 20; // Additional limit for stars
     /* 
     NUMBER OF STARS IN DIFFERENT VERSIONS
 	28 - version 1.0
         36 - version 1.1
         45 - version 1.2
         55 - version 1.3
+        ? - version 1.4 (I'm dont have this version)
+        ? - version 1.5 (I'm dont have this version)
+        102 - version 1.6
     */
-    $scoreLimitDemons = 0 + 3; // Additional limit for demons
+    $scoreLimitDemons = 1 + 3; // Additional limit for demons
     /* 
     NUMBER OF DEMONS IN DIFFERENT VERSIONS
         0 - version 1.3
+        1 - version 1.6
     */
-
-    $ratedCPs = 1; // The number of creator points for rating the level
 
     $commandRateLimit = true; // false - disable command rate limit; true - enable command rate limit
     $commandRateLimitCheckStars = true; // false - disable checking stars for difficulty; true - enable checking stars for difficulty
