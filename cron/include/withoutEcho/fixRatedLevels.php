2e27e6155c253577e287c08e7b8dbd0285e8ae50
DeXotik <95002001+DeXotik@users.noreply.github.com>
2021-12-27 19:06:08 +0200

    Delete cron directory

--- cron/include/withoutEcho/fixRatedLevels.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include dirname(__FILE__)."/../../../components/include/database.php";
-
-    $query = $db->prepare("UPDATE levels SET rated = 0 WHERE stars = 0 AND rated = 1");
-    $query->execute();
-
-    $query = $db->prepare("UPDATE levels SET rated = 1 WHERE stars > 0 AND rated = 0");
-    $query->execute();
-?>
